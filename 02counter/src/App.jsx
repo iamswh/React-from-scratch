0e7a00f57244f3067cc900447aec1745850c2cb6
Syed Wajid Hossain <wajidhossain2017@gmail.com>
2023-12-14 23:56:07 +0530

    useState hook

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  let [counter, setCounter] = useState(5);
+
+  // let counter = 5;
+  let addValue = () => {
+    if (counter < 20) {
+      //if condition is used for counter not exceeding 20
+      setCounter(counter + 1);
+    }
+    // console.log("clicked", counter);
+  };
+
+  let removeValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+
+  return (
+    <>
+      <h1>Chai aur React</h1>
+      <h2>Counter value : {counter}</h2>
+
+      <button onClick={addValue}>Add Value {counter} </button>
+      <br />
+      <button onClick={removeValue}>Remove Value {counter} </button>
+      <p>Counter: {counter}</p>
+    </>
+  );
+}
+
+export default App;
