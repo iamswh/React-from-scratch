5c0e2e76cf540201b6a821d380b39290d1421d1e
Syed Wajid Hossain <wajidhossain2017@gmail.com>
2024-01-07 20:29:02 +0530

    context api project 1

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from "react";
 
-import "./App.css";
+import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
+  
+
   return (
-    <>
-      <h1>React with Chai </h1>
-    </>
-  );
+    <UserContextProvider>
+      <h1>React with Chai and share is important</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
 }
 
-export default App;
+export default App
